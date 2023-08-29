daa85bc915469645105ce33f4c48abf5ffea4b8b
adeland <adelandluo1@gmail.com>
2023-08-28 23:39:25 -0400

    Create Majority Element II.py

---
+++ Majority Element II.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        from collections import Counter
+        dic = Counter(nums)
+        z = len(nums) // 3
+        x = list(dic.keys())
+        y = list(dic.values())
+        a = []$
+        count = -1
+        for i in y:
+            count += 1
+            if i > z:
+                a.append(x[count])
+        return a
