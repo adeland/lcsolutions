a6d35bd44e0ed55f4c9d8cff9d578723b77807a3
adeland <adelandluo1@gmail.com>
2023-09-03 17:07:32 -0400

    Create First Missing Positive.py

---
+++ First Missing Positive.py
@@ -0,0 +1,18 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+        for i in range(n):
+            if nums[i] < 1 or nums[i] > n:
+                nums[i] = n + 1
+        for i in range(n):
+            val = abs(nums[i])
+            if val > n:
+                continue
+            val -= 1
+            if nums[val] > 0: 
+                nums[val] = -1 * nums[val]
+        for i in range(n):
+            if nums[i] >=0:
+                return (i + 1)
+        return n + 1
+      # NOT MY CODE
