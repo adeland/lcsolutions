280a0b018349514b25d456757e7fb1a1b42940cb
adeland <adelandluo1@gmail.com>
2023-09-17 15:39:47 -0400

    Create Maximum Subarray.py

---
+++ Maximum Subarray.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        if nums == []:
+            return 0
+        
+        max_sum = nums[0]
+        current_sum = nums[0]
+        
+        for i in nums[1:]:
+            current_sum = max(i, current_sum + i)
+            max_sum = max(max_sum, current_sum)
+        
+        return max_sum
+        
