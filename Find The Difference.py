4ff8f78b9200c189357a4dd553a4f5137bc8ec51
adeland <adelandluo1@gmail.com>
2023-09-24 23:53:19 -0400

    Create Find The Difference.py

---
+++ Find The Difference.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        if len(t) == 1:
+            return t
+        t = list(t)
+        for i in s:
+            if i in t:
+                t.remove(i)
+        t = "".join(t)
+        return t
