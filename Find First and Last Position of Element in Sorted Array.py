5162f21b22e78bad55b5bcd0ed5f31f1cfa6ce33
a <adelandluo1@gmail.com>
2023-10-09 18:53:43 -0400

    Update Find First and Last Position of Element in Sorted Array.py

--- Find First and Last Position of Element in Sorted Array.py
+++ Find First and Last Position of Element in Sorted Array.py
@@ -1,18 +1,33 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        x = []
-        z = nums.count(target)
-        if target not in nums:
-            return [-1,-1]
-        if z == 1:
-            return [nums.index(target),nums.index(target)]
-        if z == 2:
-            x.append(nums.index(target))
-            nums.remove(target)
-            x.append(nums.index(target) + 1)
-        if z > 2:
-            x.append(nums.index(target))
-            nums = nums[::-1]
-            y = nums.index(target)
-            x.append(len(nums) - y - 1)
-        return x
+        def first(nums, target):
+            left = 0
+            right = len(nums) - 1
+            while left <= right:
+                center = left + (right - left) // 2
+                if nums[center] == target:
+                    if center == 0 or nums[center - 1] < target:
+                        return center
+                    right = center - 1
+                elif nums[center] < target:
+                    left = center + 1
+                else:
+                    right = center - 1
+            return -1
+                
+        def last(nums, target):
+            left = 0
+            right = len(nums) - 1
+            while left <= right:
+                center = left + (right - left) // 2
+                if nums[center] == target:
+                    if center == len(nums) - 1 or nums[center + 1] > target:
+                        return center
+                    left = center + 1
+                elif nums[center] < target:
+                    left = center + 1
+                else:
+                    right = center - 1
+            return -1
+
+        return [first(nums, target), last(nums, target)] 
