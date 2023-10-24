a0e94edbdcaa0fef65933583393100738eb55719
a <adelandluo1@gmail.com>
2023-10-24 05:00:45 -0500

    Update Reverse Integer.py

--- Reverse Integer.py
+++ Reverse Integer.py
@@ -1,39 +1,24 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        x = list(str(x))
-        x = x[::-1]
+        if not x:
+            return 0
+
+        x = list(str(x))[::-1]
+
+        if x[-1] == "-":
+            x.pop()
+            x.insert(0,"-")
+
         count = 0
-        if "-" in x:
-            x.remove("-")
-            for i in x:
-                z = x.index(i)
-                if i == "0":
-                    count += 1
-                elif i != "0":
-                    break
-            n = x[-1]
-            x.append(n)
-            x = x[count:-1]
-            x = x[::-1]
-            x.append("-")
-            x = x[::-1]
-            x = int("".join(x))
-            if x not in range(-2**31, 2**31):
-                return 0
-            return x
+
         for i in x:
-            z = x.index(i)
             if i == "0":
                 count += 1
-            elif i != "0":
+            else:
                 break
-        n = x[-1]
-        x.append(n)
-        x = x[count:-1]
-        x = "".join(x)
-        if x == "":
-            return 0
-        x = int(x)
-        if x not in range(-2**31, 2**31):
-            return 0
-        return x
+
+        x = int("".join(x[count:]))
+        
+        if x >= -2**31 and x <= 2**31:
+            return x
+        return 0
