276d68a8a7ed52dbec60f3009712270afb28a327
a <adelandluo1@gmail.com>
2023-10-29 18:52:41 -0400

    Update Valid Anagram.py

--- Valid Anagram.py
+++ Valid Anagram.py
@@ -1,10 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s = list(s) 
-        t = list(t)
-        s.sort()
-        t.sort()
-        if s == t:
-            return True
-        else:
-            False
+        return sorted(s) == sorted(t)
