1cdfe3879df114432b2db5deaaf48c4025c9599a
a <adelandluo1@gmail.com>
2023-10-29 22:57:54 -0400

    Update Group Anagrams.py

--- Group Anagrams.py
+++ Group Anagrams.py
@@ -1,11 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        x = {}
+        seen = {}
         for i in strs:
-            y = "".join(sorted(i))
-            if y in x:
-                x[y].append(i)
-            else:
-                x[y] = [i]
-        return list(x.values())
-        
+            word = "".join(sorted(i))
+            if word not in seen:
+                seen[word] = []
+            seen[word].append(i)
+        return list(seen.values())
