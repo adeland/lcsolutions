cd87688b1f1e0a66b56061ec6c22277dd76ca7c0
a <adelandluo1@gmail.com>
2024-01-21 19:41:01 -0500

    Create Set Mismatch.py

---
+++ Set Mismatch.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+
+        nums = sorted(nums)
+        checks = set(nums)
+        values = Counter(nums)
+        
+        for check in list(range(1, len(nums) + 1)):
+            if check not in checks:
+                error = check
+                break
+
+        for check in values:
+            if values.get(check) == 2:
+                ret = check
+
+        return [ret, error]
